da8e6ea0c522685d18d85d6c1ab892b40bc2f264
Suhwan0818 <tnghks1997@naver.com>
2023-08-28 23:40:53 +0900

    🎉 프로젝트 시작

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    swc: {
+      jsc: {
+        parser: {
+          syntax: "ecmascript",
+          jsx: true,
+          dynamicImport: true
+        }
+      }
+    },
+    styledComponents: true, // Next에게 styled-component도 처리해달라고 옵션을 설정해줌
+  },
+};
 
 module.exports = nextConfig
